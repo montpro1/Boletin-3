dbf9dbfa35ea0ab2a60ff1b8997fc4f7e8c0c6bf
Víctor Enrique Montes Tovar <vmontestovar@nautilus19.ad.danielcastelao.org>
2019-10-09 13:39:59 +0200

    FINALIZADO 2

---
+++ b3_012/src/b3_012/B3_012.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b3_012;
+
+/**
+ *
+ * @author vmontestovar
+ */
+public class B3_012 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
